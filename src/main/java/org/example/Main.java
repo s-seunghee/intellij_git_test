6315d75cd4671b0f65b7f12d9d175503665995f7
shshin <shs07230@gmail.com>
2024-01-29 14:47:34 +0900

    Commit and Push Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Git Test");
+        System.out.println("Commit and Push Test");
     }
 }
