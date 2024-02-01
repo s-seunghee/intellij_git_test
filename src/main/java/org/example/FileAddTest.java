dc7a2f2ac13a50566e5f935891d8f173c468e316
gwangu <dagger8238@naver.com>
2024-02-01 17:23:25 +0900

    테스트 커밋

--- src/main/java/org/example/FileAddTest.java
+++ src/main/java/org/example/FileAddTest.java
@@ -2,5 +2,6 @@ package org.example;
 
 public class FileAddTest {
     //안녕하세요
+    int asd = 1;
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Commit and Push Test");
     }
+    //안녕하세요
 }
